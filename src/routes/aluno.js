2f07a1eb7f4c639c5a02703d8785d492e289230d
Xar0P <pietroricardocres@hotmail.com>
2021-11-29 18:40:16 -0300

    CRUD alunos

--- src/routes/aluno.js
+++ src/routes/aluno.js
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import alunoController from '../controllers/Aluno';
 
+import loginRequired from '../middlewares/loginRequired';
+
 const router = new Router();
 
 router.get('/alunos/', alunoController.index);
+router.post('/alunos/', loginRequired, alunoController.store);
+router.put('/alunos/:id', loginRequired, alunoController.update);
+router.get('/alunos/:id', alunoController.show);
+router.delete('/alunos/:id', loginRequired, alunoController.delete);
 
 export default router;
