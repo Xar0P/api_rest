0c75fe591d24f2a224c52e24006cbd3a21f1db96
Xar0P <pietroricardocres@hotmail.com>
2021-11-30 17:05:24 -0300

    db to photos

--- src/controllers/Photo.js
+++ src/controllers/Photo.js
@@ -1,16 +1,22 @@
 import multer from 'multer';
 import multerConfig from '../config/multer';
 
+import Photo from '../models/Photo';
+
 const upload = multer(multerConfig).single('photo');
 
 class PhotoController {
-  async store(req, res) {
-    return upload(req, res, (err) => {
+  store(req, res) {
+    return upload(req, res, async (err) => {
       if (err) {
         return res.status(400).json({
           errors: [err.code],
         });
       }
+
+      const { originalname, filename } = req.file;
+      const { aluno_id } = req.body;
+      const photo = await Photo.create({ originalname, filename, aluno_id });
 
       return res.json(req.file);
     });
--- src/routes/photo.js
+++ src/routes/photo.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 
 import photoController from '../controllers/Photo';
+import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
-router.post('/photos/', photoController.store);
+router.post('/photos/', loginRequired, photoController.store);
 
 export default router;
