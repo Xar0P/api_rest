d6beaac43bb3bb260cf3bfd5c9e2c1315b456c91
Xar0P <pietroricardocres@hotmail.com>
2021-11-30 15:05:23 -0300

    validations upload

--- src/routes/photo.js
+++ src/routes/photo.js
@@ -1,13 +1,9 @@
 import { Router } from 'express';
-import multer from 'multer';
 
 import photoController from '../controllers/Photo';
-import multerConfig from '../config/multer';
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
-router.post('/photos/', upload.single('photo'), photoController.store);
+router.post('/photos/', photoController.store);
 
 export default router;
